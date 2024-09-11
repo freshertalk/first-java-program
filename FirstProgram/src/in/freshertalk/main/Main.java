a9471d350328e8a3cdd6ed3cb9f362ee6ca8534d
freshertalk <freshertalk.in@gmail.com>
2024-09-11 09:06:50 +0530

    first commit

---
+++ FirstProgram/src/in/freshertalk/main/Main.java
@@ -0,0 +1,11 @@
+package in.freshertalk.main;
+
+//This is a basic Java Program that prints "Hello FresherTalk!" to the console
+public class Main {
+
+	// The main() method is entry point of any Java application. 
+	public static void main(String[] args) {
+		// The below line prints "Hello FresherTalk!" to the console 
+		System.out.println("Hello FresherTalk");
+	}
+}
