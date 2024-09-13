da852dc2c7d9913a642f3e0a15383b9d52fffb5e
vinay-tiwari-6 <vinaytiwari.iist@gmail.com>
2024-09-13 13:23:20 +0530

    added comments

--- FirstProgram/src/in/freshertalk/main/Main.java
+++ FirstProgram/src/in/freshertalk/main/Main.java
@@ -2,16 +2,18 @@ package in.freshertalk.main;
 
 //This is a basic Java Program that prints "Hello FresherTalk!" to the console
 /**
- * Author: Vinay Kumar Tiwari
+ * Welcome to FresherTalk Technologies 
+ * Author: Vinay Kumar Tiwari 
  * Date: 11th September 2024
+ * Email: vinay.tiwari@freshertalk.in
  * 
  */
 public class Main {
 
-	// The main() method is entry point of any Java application. 
+	// The main() method is entry point of any Java application.
 	public static void main(String[] args) {
-		
-		// The below line prints "Hello FresherTalk!" to the console 
-		System.out.println("Hello FresherTalk!");
+
+		// The below line prints "Hello, FresherTalk!" to the console
+		System.out.println("Hello, FresherTalk!");
 	}
 }
